651b5062b5d3f921454ab08700bd1bd10e224542
LongxianChen <544013178@qq.com>
2023-05-02 23:24:45 +0800

    update

---
+++ Docs/Get_trained_sites.pl
@@ -0,0 +1,38 @@
+#! /usr/bin/perl
+use strict;
+
+die "perl <ref_transcriptome.fa> <trained_motif> <trained_site.bed> \n" if (@ARGV!=3);
+
+my($ref,$motif,$out)=@ARGV;
+
+open(REF,$ref) || die "$!";
+open(MOTIF,$motif) || die "$!";
+open(EXPORT,">$out") || die "$!";
+
+my @trained_motif;
+
+while(<MOTIF>) {
+    chomp;
+    push(@trained_motif,$_);
+}
+
+$/="\>";
+<REF>;
+while(<REF>){
+    chomp;
+    my($trans_line,$seq) = split /\n/, $_, 2;
+    my $trans = (split / /, $trans_line)[0];
+    $seq =~ s/[\n]//g;
+    my $length = length($seq);
+    for(my $i=4; $i<=$length-2; $i++){
+        my $motif = substr($seq,$i-4,6);
+        if(grep (/$motif/, @trained_motif)){
+            print EXPORT $trans,"\t",$i,"\t",$motif,"\n";
+        }
+    }
+}
+
+close REF;
+close MOTIF;
+close EXPORT;
+exit;
